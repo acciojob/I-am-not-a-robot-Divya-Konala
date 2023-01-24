bba58e65e364511e7791205c607643dd55affd35
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-01-24 21:37:52 +0530

    shuffled images

--- script.js
+++ script.js
@@ -1,72 +1,81 @@
 //your code here
-let images= document.getElementsByClassName("given_img");
-let img=[];
-let reset_btn=document.getElementById("reset");
-reset_btn.disabled=true;
-let verify_btn=document.getElementById("verify");
-verify_btn.disabled=true;
-let img_btns=document.getElementsByClassName("img_btn");
-let noOfImgsSelected=0;
-let image1_url;
-let image2_url;
-let img1_id;
-let img2_id;
-for(let i=0;i<images.length;i++){
-    img[i]=(window.getComputedStyle(images[i]).content).slice(5,-2);
-}
+// --------------creating elements---------------
+h3Ele=document.createElement("h3");
+h3Ele.textContent="Please click on the identical tiles to verify that you are not a robot.";
+h3Ele.setAttribute("id","h");
+document.querySelector("body").append(h3Ele);
 
-function randomImage(){
-   let x= Math.floor(Math.random()*images.length);
-//    document.querySelector(".img6").src=img[x];
-   document.querySelector(".img6").style.content='url("'+img[x]+'")';
-}
-randomImage();
+resetBtn=document.createElement("button");
+resetBtn.textContent="reset";
+resetBtn.setAttribute("id","reset");
+resetBtn.style.display="none";
+document.querySelector("body").append(resetBtn);
 
-for(let i=0;i<img_btns.length;i++){
-    img_btns[i].addEventListener("click",()=>{verify_img(i)});
-}
- 
-function verify_img(btn_id) {
-    noOfImgsSelected++;
-    if(btn_id<img_btns.length-1){
-        image1_url=img[btn_id];
-    }else if(btn_id===img_btns.length-1){
-        // image2_url=document.querySelector(".img6").src;
-        image2_url=(window.getComputedStyle(document.querySelector(".img6")).content).slice(5,-2);
+verifyBtn=document.createElement("button");
+verifyBtn.textContent="Verify";
+verifyBtn.setAttribute("id","verify");
+verifyBtn.style.display="none";
+document.querySelector("body").append(verifyBtn);
+
+msg=document.createElement("p");
+msg.setAttribute("id","para");
+document.querySelector("body").append(msg);
+
+// ------------shuffling images-----------------
+let imgArr=["img1","img2","img3","img4","img5"];
+let shuffledArr=[];
+let randImgNum=Math.floor(Math.random()*imgArr.length);
+imgArr.push(imgArr[randImgNum]);
+
+let imgElements=document.querySelectorAll("img");
+
+while(imgArr.length!=0){
+    let image=imgArr.pop();
+    while (true) {
+        let randNum=shuffleImages();
+        if(shuffledArr[randNum]==undefined){
+            shuffledArr[randNum]=image;
+            imgElements[randNum].setAttribute("class",image);
+            break;
+        }   
     }
-    if(noOfImgsSelected===1){
-        img1_id=btn_id;
-        reset_btn.disabled=false;
-    }else if(noOfImgsSelected===2){
-        if(img1_id===btn_id){
-            noOfImgsSelected--;
+}
+function shuffleImages(){ 
+    let randNum=Math.floor(Math.random()*imgElements.length);
+    return randNum;
+}
+// -----------
+let clickedImgsIndexNum=[];
+let numOfClicks=0;
+for(let i=0;i<imgElements.length;i++){
+    imgElements[i].addEventListener("click",()=>{
+        numOfClicks++;
+        clickedImgsIndexNum.push(i);
+        if(numOfClicks==1) {
+            resetBtn.style.display="inLine";
+        }else if(numOfClicks==2){
+            verifyBtn.style.display="inLine";
         }else{
-            img2_id=btn_id;
-            verify_btn.disabled=false;
+            verifyBtn.style.display="none";
         }
-    }else if(noOfImgsSelected>2){
-        verify_btn.disabled=true;
+    });
+}
+
+document.querySelector("#verify").addEventListener("click",()=>{
+    let img1=imgElements[clickedImgsIndexNum[0]].getAttribute("class");
+    let img2=imgElements[clickedImgsIndexNum[1]].getAttribute("class");
+    if(clickedImgsIndexNum[0]!=clickedImgsIndexNum[1] && img1==img2){
+        document.querySelector("#para").textContent="You are a human. Congratulations!";
+        document.querySelector("#para").style.color="green";
+    }else{
+        document.querySelector("#para").textContent="We can't verify you as a human. You selected the non-identical tiles.";
+        document.querySelector("#para").style.color="red";
     }
-}
+})
 
-
-function reset(){
-    image1_url="";
-    image2_url="";
-    noOfImgsSelected=0;
-    reset_btn.disabled=true;
-    verify_btn.disabled=true;
-    document.getElementById("para").innerText="";
-}
-
-function verify(){
-    let msg;
-    if(image1_url==image2_url)
-        msg="Congratulations!";
-    else{
-        msg="We can't verify you as a human. You selected the non-identical tiles.";
-    }
-    document.getElementById("para").innerText=msg;
-    reset_btn.disabled=true;
-    verify_btn.disabled=true;
-}
+document.querySelector("#reset").addEventListener("click",()=>{
+    clickedImgsIndexNum=[];
+    numOfClicks=0;
+    resetBtn.style.display="none";
+    verifyBtn.style.display="none";
+})
